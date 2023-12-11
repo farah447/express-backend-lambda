fccd96c5912183823e865d997377e3beb3131152
Farah <alharbi.farah@hotmail.com>
2023-12-11 16:42:16 +0300

    changes

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URL);
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log('Database connected');
     } catch (error) {
         console.log("error in db");
